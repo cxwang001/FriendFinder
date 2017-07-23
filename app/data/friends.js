6fe3eb483f7fa166eb5eda7b668978734d832b29
cxwang001 <william@Williams-MBP.attlocal.net>
2017-07-22 23:40:03 -0700

    apiRousting

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,20 @@
-var friendsArray = [];
+var friendsArray = [
+{
+"name": "Jacob Deming",
+"photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+"scores": [
+"4",
+"2",
+"5",
+"1",
+"3",
+"2",
+"2",
+"1",
+"3",
+"2"]
+},
+];
 
 
 // Note how we export the array. This makes it accessible to other files using require.
